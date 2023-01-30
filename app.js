63e6278afb7ea984a2d9dfd0307214f37a32de12
whovivekshukla <vivekshuklahere@gmail.com>
2023-01-30 15:37:14 +0530

    update

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
+
+// database
+const connectDB = require("./db/connect");
+
+// routers
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Subevel");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use(notFoundMiddleware);
 
-app.listen(PORT, console.log(`Server is listening at ${PORT}`));
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening at ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
